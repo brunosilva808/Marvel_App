3cb6d25a4483d1b3558971a6831a272171beacf2
brunosilva808 <bsimoesmail@gmail.com>
2018-12-02 18:04:42 +0000

    * search bar implemented

--- Marvel/Controller/HomeViewController.swift
+++ Marvel/Controller/HomeViewController.swift
@@ -11,33 +11,44 @@ private let reuseIdentifier = "Cell"
 
 class HomeViewController: UIViewController {
 
-    @IBOutlet weak var tableView: UITableView! {
-        didSet {
-            tableView.delegate = self
-            tableView.dataSource = self
-            tableView.estimatedRowHeight = 44
-            tableView.rowHeight = UITableView.automaticDimension
-            tableView.registerNib(for: CharacterCell.self)
-        }
-    }
+    private var searchController = UISearchController(searchResultsController: nil)
+    @IBOutlet weak var tableView: UITableView!
     
-    var character: Character? {
-        didSet {
-            DispatchQueue.main.async { [weak self] in
-                self?.tableView.reloadData()
-            }
-        }
-    }
+    private var character: Character?
+    private var filterCharacters = [Result]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupSearchController()
         setupTableView()
+        
         NetworkManager().getCharacters(page: 0, onSuccess: { [weak self] (response) in
             self?.character = response
+            self?.filterCharacters = response.data.results
+            
+            DispatchQueue.main.async { [weak self] in
+                self?.tableView.reloadData()
+            }
         }) {
             
         }
+    }
+    
+    func setupSearchController() {
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Search"
+        navigationItem.searchController = searchController
+        definesPresentationContext = true
+    }
+    
+    func setupTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.estimatedRowHeight = 44
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.registerNib(for: CharacterCell.self)
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -46,24 +57,49 @@ class HomeViewController: UIViewController {
 //            destinationViewController.result = comics?.data.results[indexPath.row]
         }
     }
-    
-    func setupTableView() {
-        
-    }
 
+    // MARK: - Private instance methods
+    
+    private func searchBarIsEmpty() -> Bool {
+        return searchController.searchBar.text?.isEmpty ?? true
+    }
+    
+    private func filterContentForSearchText(_ searchText: String, scope: String = "All") {
+        if let results = character?.data.results {
+            filterCharacters = results.filter({( result : Result) -> Bool in
+                return result.name.lowercased().contains(searchText.lowercased())
+            })
+            
+            tableView.reloadData()
+        }
+    }
+    
+    private func isFiltering() -> Bool {
+        return searchController.isActive && !searchBarIsEmpty()
+    }
 }
 
 extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return character?.data.results.count ?? 0
+        return filterCharacters.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: CharacterCell.reuseIdentifier, for: indexPath) as! CharacterCell
-        cell.model = character?.data.results[indexPath.row]
+        cell.model = filterCharacters[indexPath.row]
         return cell
-//        return tableView.reusableCell(for: indexPath.row, with: character?.data.results[indexPath.row]) as CharacterCell
+//        return tableView.reusableCell(for: indexPath.row, with: filterCharacters[indexPath.row]) as CharacterCell
     }
     
+}
+
+extension HomeViewController: UISearchResultsUpdating {
+
+    func updateSearchResults(for searchController: UISearchController) {
+        if let text = searchController.searchBar.text {
+            filterContentForSearchText(text)
+        }
+    }
+
 }
